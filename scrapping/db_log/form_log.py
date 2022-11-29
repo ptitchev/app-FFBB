848e4c3779fbdbe2108bd685251813205299a9f7
Jules Chevenet <74178881+ptitchev@users.noreply.github.com>
2022-11-29 12:57:29 +0100

    Add files via upload

---
+++ scrapping/db_log/form_log.py
@@ -0,0 +1,6 @@
+def log_info():
+    print("Saisir username Postgre (par défault : postgres) : ")
+    username = input()
+    print("Saisir password Postgre : ")
+    password = input()
+    return username, password
